ed465b611ee871617bdf71c26e8fdbaf19350dc8
Ben Pardo <benpardo@bens-mbp-4.lan>
2017-09-23 18:39:28 -0600

    Tests a normal leap year.

--- leap.test.js
+++ leap.test.js
@@ -1,7 +1,12 @@
 let {store} = require("./leap-store.js");
 const {changeYear} = require("./change-leap-year-action");
 
-test("It checks something basic.", function () {
+test("It checks a year which is not a leap year.", function () {
     store.dispatch(changeYear(1997));
     expect(store.getState().isLeap).toBe(false);
+});
+
+test("A year that is a normal leap year", function () {
+    store.dispatch(changeYear(2004));
+    expect(store.getState().isLeap).toBe(true);
 });
